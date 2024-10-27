a862650e15573a3ff1511ba9326893ba07b6cf8d
propertymkononi <158148414+propertymkononi@users.noreply.github.com>
2024-10-27 22:49:41 +0300

    new tenant js file

---
+++ tenant.js
@@ -0,0 +1,26 @@
+// list of tenants
+const tenantList = ["tenant1", "tenant2"];
+
+
+// tenant information
+const tenant = {
+    tenantName:"",
+    houseNumber:"",
+    phoneNumber:"",
+    apartment:"",
+    rentStatus:"",
+    tenancyDuration:"",
+    houseCondition:"",
+    leaseDate:"",
+    emailAddress:"",
+    idNumber:"",
+    employmentStatus:"",
+    reference1Name:"",
+    reference1Contact:"",
+    reference1ID:"",
+    reference2Name:"",
+    reference2Contact:"",
+    reference2ID:"",
+};
+
+
